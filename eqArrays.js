259391caa081ba820b53fc657009e970cb7ebe46
CJM1994 <connorjohnmullin@gmail.com>
2022-02-08 23:58:55 +0000

    add eqArrays.js to compare if arrays are identical

---
+++ eqArrays.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else return console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+assertEqual(eqArrays([], []), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), true); // => should FAIL
+assertEqual(eqArrays([1, 'f', 3], [1, 2, 3]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 8], [1, 2, 3]), true); // => should FAIL
