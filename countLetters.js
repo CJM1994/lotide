a900322b34be03d1bedf42dfcb55ebc606e292a6
CJM1994 <connorjohnmullin@gmail.com>
2022-02-09 21:18:01 +0000

    lint

--- countLetters.js
+++ countLetters.js
@@ -1,17 +1,17 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     return console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
   } else return console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
 };
 
-const countLetters = function (sentence) {
+const countLetters = function(sentence) {
   let parsedSentence = sentence.split(' ').join('');
   const returnObject = {};
   for (const letter of parsedSentence) {
     if (!returnObject[letter]) returnObject[letter] = 1;
-    else returnObject[letter]++
+    else returnObject[letter]++;
   }
   return returnObject;
-}
+};
 
 console.log(countLetters('This is an example.'));
