0dd95dec51f91bf7a53308fd88aa1d3244b648cf
CJM1994 <connorjohnmullin@gmail.com>
2022-02-08 16:06:23 -0800

    no significant changes

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,10 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     return console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
   } else return console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
 };
 
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   if (array1.length !== array2.length) return false;
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) return false;
