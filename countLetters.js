9a5b82e5ca5bba75d2d38f7c9e699f1f3f390a87
CJM1994 <connorjohnmullin@gmail.com>
2022-02-09 21:17:33 +0000

    add countletters function to countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,17 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else return console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+const countLetters = function (sentence) {
+  let parsedSentence = sentence.split(' ').join('');
+  const returnObject = {};
+  for (const letter of parsedSentence) {
+    if (!returnObject[letter]) returnObject[letter] = 1;
+    else returnObject[letter]++
+  }
+  return returnObject;
+}
+
+console.log(countLetters('This is an example.'));
