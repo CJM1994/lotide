dabc64e6c7835299cc787f7a8f89747ff6b400a8
CJM1994 <connorjohnmullin@gmail.com>
2022-02-09 23:36:51 +0000

    findkeybyvalue function add & test

---
+++ findKeyByValue.js
@@ -0,0 +1,22 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else return console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+const findKeyByValue = function (object, value) {
+  const keyArray = Object.keys(object);
+  for (const key of keyArray) {
+    if (object[key] === value) return key;
+  }
+}
+
+//TESTS
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
