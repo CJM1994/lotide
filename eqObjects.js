93e24857ab7e8e0e890a91505e3a699d0446c0ce
CJM1994 <connorjohnmullin@gmail.com>
2022-02-09 15:45:31 -0800

    add tests and description

--- eqObjects.js
+++ eqObjects.js
@@ -4,6 +4,16 @@ const assertEqual = function (actual, expected) {
   } else return console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
 };
 
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
 const eqObjects = function () {
 
 }
+
+// TESTS
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // => false
