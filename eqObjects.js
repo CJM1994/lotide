891d9647630e92ea68ea872bf4f3f37e0c30b84a
CJM1994 <connorjohnmullin@gmail.com>
2022-02-09 15:40:13 -0800

    added empty function and assertequal

---
+++ eqObjects.js
@@ -0,0 +1,9 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else return console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+const eqObjects = function () {
+
+}
