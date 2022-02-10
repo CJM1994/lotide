2f5f035d9fed1e1edc32aed87353073ab3bbbcdc
CJM1994 <connorjohnmullin@gmail.com>
2022-02-10 23:45:37 +0000

    takeUntil implemented

---
+++ takeUntil.js
@@ -0,0 +1,17 @@
+const takeUntil = function (array, callback) {
+  returnArray = [];
+  for (const element of array) {
+    if (callback(element)) return returnArray;
+    returnArray.push(element);
+  }
+}
+
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+
+// console.log('---');
+
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
