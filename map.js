1ed3c7c400a49fa5bfbf92f0e34ac89fabf758c6
CJM1994 <connorjohnmullin@gmail.com>
2022-02-10 15:04:38 -0800

    added callback function

---
+++ map.js
@@ -0,0 +1,9 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (arrayToMap, callbackFunction) {
+  const results = [];
+  for (const element of arrayToMap) {
+    results.push(callbackFunction(element));
+  }
+  return results;
+}
