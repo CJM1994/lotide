d66f343635ad2424538bbe9a6e74157571c646ba
CJM1994 <connorjohnmullin@gmail.com>
2022-02-09 16:10:20 -0800

    added checks for number of keys and key values

--- eqObjects.js
+++ eqObjects.js
@@ -6,7 +6,22 @@ const assertEqual = function (actual, expected) {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function () {
+const eqObjects = function (object1, object2) {
+
+  const keyArray1 = Object.keys(object1);
+  const keyArray2 = Object.keys(object2);
+
+  console.log(object1, object2);
+
+  console.log('Equal Number of Keys in Object?');
+  assertEqual(keyArray1.length, keyArray2.length);
+  if (keyArray1.length !== keyArray2.length) return false;
+
+  console.log('Equal Key Value Pairs?')
+  for (const key of keyArray1) {
+    assertEqual(object1[key], object2[key]);
+    if (object1[key] !== object2[key]) return false;
+  }
 
 }
 
@@ -15,5 +30,5 @@ const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 eqObjects(ab, ba); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc); // => false
+// const abc = { a: "1", b: "2", c: "3" };
+// eqObjects(ab, abc); // => false
