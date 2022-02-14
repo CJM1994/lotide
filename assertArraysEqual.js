eb2bd7f859d28a109bd5974a2552bc0b098d883a
CJM1994 <connorjohnmullin@gmail.com>
2022-02-14 13:09:09 -0800

    refactored assertArraysEqual to be a module

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,4 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) return false;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false;
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -12,3 +6,5 @@ const assertArraysEqual = function (array1, array2) {
   }
   else console.log('🛑 Arrays do not match');
 };
+
+module.exports = assertArraysEqual;
