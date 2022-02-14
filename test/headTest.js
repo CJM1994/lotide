19a884110973b9f03f289b728733d9b528e14455
CJM1994 <connorjohnmullin@gmail.com>
2022-02-14 14:29:31 -0800

    modified headTest.js to use chai for test assertions

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,22 @@
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 const head = require('../head')
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head(['one']), 'one');
+describe('#HEAD', () => {
+
+  it('should return the first number in an array of numbers', () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  })
+
+  it('should return the first string in an array of strings', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  })
+
+  it('should return undefined for an empty array', () => {
+    assert.strictEqual(head([]), undefined);
+  })
+
+  it('should return the only element in an array with one element', () => {
+    assert.strictEqual(head(['one']), 'one');
+  })
+
+})
