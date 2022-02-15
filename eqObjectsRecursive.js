2a18b53051304e0417c5bcbb582b3a15d9089042
CJM1994 <connorjohnmullin@gmail.com>
2022-02-14 22:13:34 -0800

    added recursive obj checker

--- eqObjectsRecursive.js
+++ eqObjectsRecursive.js
@@ -1,17 +1,14 @@
-const eqArrays = require('./eqArrays');
-
 const eqObjectsRecursive = function (object1, object2) {
 
-  const keyArray1 = Object.keys(object1);
-  const keyArray2 = Object.keys(object2);
+  if (object1 === object2) return true;
+
+  let keyArray1 = Object.keys(object1);
+  let keyArray2 = Object.keys(object2);
 
   if (keyArray1.length !== keyArray2.length) return false;
 
-  for (const key of keyArray1) {
-    if (Array.isArray(object1[key])) {
-      if (!eqArrays(object1[key], object2[key])) return false;
-    }
-    else { if (object1[key] !== object2[key]) return false };
+  for (let key of keyArray1) {
+    if (!eqObjectsRecursive(object1[key], object2[key])) return false;
   }
 
   return true;
@@ -19,3 +16,6 @@ const eqObjectsRecursive = function (object1, object2) {
 }
 
 module.exports = eqObjectsRecursive;
+
+// Can't handle arrays like the other function yet, need to make a recursive array checker first 
+// and then will add that to this
--- test/eqObjectsRecursiveTest.js
+++ test/eqObjectsRecursiveTest.js
@@ -1,2 +1,18 @@
 const eqObjectsRecursive = require('../eqObjectsRecursive');
 const assert = require('chai').assert;
+
+describe('EQ OBJECTS RECURSIVE', () => {
+
+  it('This nested object should return true', () => {
+    assert.strictEqual(eqObjectsRecursive({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+  })
+
+  it('This nested object should return false', () => {
+    assert.strictEqual(eqObjectsRecursive({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+  })
+
+  it('This nested object should return false', () => {
+    assert.strictEqual(eqObjectsRecursive({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+  })
+
+})
