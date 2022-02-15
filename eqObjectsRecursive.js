c1274406cb4c9254b7132389eb844f278cd6b310
CJM1994 <connorjohnmullin@gmail.com>
2022-02-14 22:17:53 -0800

    comments

--- eqObjectsRecursive.js
+++ eqObjectsRecursive.js
@@ -1,5 +1,6 @@
 const eqObjectsRecursive = function (object1, object2) {
 
+  if (Array.isArray(object1) || Array.isArray(object2)) return false; // Returns false for now if an array
   if (object1 === object2) return true;
 
   let keyArray1 = Object.keys(object1);
@@ -8,6 +9,7 @@ const eqObjectsRecursive = function (object1, object2) {
   if (keyArray1.length !== keyArray2.length) return false;
 
   for (let key of keyArray1) {
+    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) return false; // Returns false for now if array
     if (!eqObjectsRecursive(object1[key], object2[key])) return false;
   }
 
