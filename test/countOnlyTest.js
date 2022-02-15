4e85a8d244413a0e6fb64eb3fed3436cf30909f2
CJM1994 <connorjohnmullin@gmail.com>
2022-02-14 20:04:02 -0800

    refactored countonly

---
+++ test/countOnlyTest.js
@@ -0,0 +1,27 @@
+const countOnly = require('../countOnly');
+assert = require('chai').assert;
+
+describe('#COUNTONLY', () => {
+
+  it('Should count Jason x1, and Fang x2', () => {
+
+    const firstNames = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+
+    expected = { Jason: 1, Fang: 2 };
+    actual = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+    assert.deepEqual(actual, expected);
+
+  })
+
+})
